b29dbcec9d46cc220954eeb27633f23b44fd77af
maryrosedevega <143856348+maryrosedevega@users.noreply.github.com>
2023-12-08 18:45:21 +0800

    Delete EAD directory

--- EAD/copy_master_files.py
+++
@@ -1,8 +0,0 @@
-from aspace_prep.copy_eads import copy_eads
-
-vandura_base_dir = 'C:/Users/djpillen/GitHub/vandura'
-vandura_real_masters_all = 'C:/Users/djpillen/GitHub/vandura/Real_Masters_all'
-aspace_ead_dir = 'eads'
-
-copy_eads(vandura_base_dir, aspace_ead_dir)
-copy_eads(vandura_real_masters_all, aspace_ead_dir)
